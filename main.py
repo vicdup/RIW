c27a9aabe93571677aed22c118ede7076a59144b
vicdup <vicdup@gmail.com>
2015-11-23 17:21:30 +0100

    toDico

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 
-
 def toDico(path):
 	articles = {}
 	article = {}
@@ -14,12 +13,34 @@ def toDico(path):
 				if (id<>-1):
 					articles[id] = article
 					article={}
-			 	id = int(line [3:])
+				id = int(line [3:])
 			elif (line[0:2] in separateurs):
 				delimiteur = line[0:2]
 			elif (line[0:2] not in separateurs and id <>-1 and delimiteur <>'.I'):
-				article[delimiteur]=line.replace('\n', ' ')
+				if (delimiteur in article.keys()):
+					if (delimiteur == '.A'):
+						article[delimiteur]=article[delimiteur]+[line.replace('\n', '')]
+					else:
+						article[delimiteur]=article[delimiteur]+line.replace('\n', ' ')
+				else:
+					if (delimiteur == '.A'):
+						article[delimiteur]=[line.replace('\n', '')]
+					else:
+						article[delimiteur]=line.replace('\n', ' ')
 		articles[id] = article
 	return articles
 
-print toDico('CACM\cacm.all')[3204]
+def toListCW(comonwords):
+	lComonWords =[]
+	with open(comonwords, 'r') as CW:
+		for word in CW:
+			lComonWords+=[word[:-1]];
+	return lComonWords
+
+def createVocab(cacmfile, comonwords):
+	vocabulaire=[]
+	
+	return vocabulaire
+
+print toDico('CACM\cacm.all')[1]['.A']
+# print toListCW('CACM\common_words')
