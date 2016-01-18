7120b0a698dc6060c9aca59364916c903ca6e46c
Victor <vicdup@gmail.com>
2016-01-18 19:58:08 +0100

    fixed do not take into account query we dont have pertinent results for

--- execute.py
+++ execute.py
@@ -10,45 +10,6 @@ sourceEval = 'CACM\query.text'
 source = 'CACM\cacm.all'
 commonwords = 'CACM\common_words'
 
-# index.generateInverseIndex()
-# print index.iIndex
-# print sys.getsizeof(index.iIndex)
-# print json.dumps(index.iIndex)
-# print len(index.iIndex.keys())
-
-# print json.dumps(index.iIndex)
-# print search.booleanSearch(index.iIndex)
-# vectorA = {"victor": 10, "pauline": 5, "matthieu": 3, "aurore": 12}
-# vectorB = {"victor": 2, "pauline": 0, "matthieu": 3, "aurore": 12}
-
-# print search.calculCosinus(vectorA,vectorB)
-# index.generateIIndex()
-# index.generateIndex()
-# import operator
-# print index.generateDico()[398]['.T']
-
-
-
-
-
-
-
-
-
-
-# print json.dumps(index.generateDico())
-# index.generateIIndex()
-# index.generateIndex()
-# print json.dumps(index.index)
-# print index.iIndex['computer']['total']
-# index.loadIIndexFromFile()
-# search = search.Search('tf-idf')
-# search.setQuery('machine OR program AND logical AND loading NOT boolean')
-# search.setQuery('Intermediate languages used in construction of multi-targeted compilers; TCOLL')
-# search.presentResults(search.vectorielSearch(index.iIndex))
-# print search.booleanSearch(index.iIndex)
-# print json.dumps(index.iIndex, sort_keys=True, indent=4, separators=(',', ': '))
-
 ####
 ####
 # Generation de l'index sur CACM
@@ -100,8 +61,9 @@ for requete in evaluation.dico:
 	for k in [1,5,10,20]:
 		# print results
 		# print parsedQrels[requete]
-		resultatsEvaluation[requete][k]['rappel'] = calculRappel(results, parsedQrels[requete],k) 
-		resultatsEvaluation[requete][k]['precision'] = calculPrecision(results, parsedQrels[requete],k) 
+		if parsedQrels[requete]:
+			resultatsEvaluation[requete][k]['rappel'] = calculRappel(results, parsedQrels[requete],k) 
+			resultatsEvaluation[requete][k]['precision'] = calculPrecision(results, parsedQrels[requete],k) 
 header = ['requete','ordre','rappel','precision']
 f.writerow(header)
 for requete in resultatsEvaluation:
